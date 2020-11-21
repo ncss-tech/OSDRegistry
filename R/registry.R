b7e264a6122f0ab06b674ecb6bf3100a37914b09
Andrew G. Brown <andrew.g.brown@usda.gov>
2020-11-21 07:03:31 -0800

    fix path problems

--- R/registry.R
+++ R/registry.R
@@ -1,35 +1,44 @@
 #' Refresh the repository/package data by automating regional queries to soil series server
-#' @return Text files written to alphabetical folders containing raw Official Series Description text. Derivative calculations output to X. Etc.
+#' @description Thank goodness we don't need to read .doc files... the server delivers .txt masquerading as .doc.
+#' @return Text files written to alphabetical folders containing raw Official Series Description (OSD) text. For use in automatic pipeline to regularly commit changes across the entire set of OSDs.
 #' @export
-#' @importFrom textreadr read_doc
+#' @importFrom utils unzip
 refresh_registry <- function() {
   
   # message("Downloading data...")
   
   # TODO: download ZIPs (pain in the butt; automate ASPX form submission by region?)
-  cat(getwd())
   message("Refreshing OSDs...")
   
   # unzip to single directory of .doc files
   
-  lapply(file.path("raw", list.files("raw", "zip", ignore.case = TRUE)), unzip, exdir = "raw/doc")
+  lapply(file.path("raw", 
+                   list.files("raw", "zip", 
+                              recursive = TRUE, 
+                              ignore.case = TRUE)), 
+         unzip, exdir = "raw/doc")
   
   # read .doc files
   
-  docfiles <-  list.files("raw/doc", "doc$")
-  docletters <- substr(docfiles, 0, 1)
+  docfiles <-  list.files("raw/doc", "doc$", recursive = TRUE)
+  docletters <- toupper(substr(basename(docfiles), 0, 1))
   
   osds <- file.path("raw/doc", docfiles)
   
   osdlist <- split(unlist(osds), docletters)
   
-  # cat(names(osdlist))
-  
   lapply(file.path("OSD", LETTERS), function(x) if (!dir.exists(x)) dir.create(x, recursive = TRUE))
   
   result <- lapply(1:length(LETTERS), function(i) {
       lapply(osdlist[[LETTERS[i]]], function(f) {
-          write((textreadr::read_doc(f)), file.path("OSD", LETTERS[i], gsub("\\.doc", "\\.txt", basename(f))))
+          suppressWarnings({
+            write(readLines(f), 
+                  file.path("OSD", 
+                            LETTERS[i], 
+                            gsub("\\.doc", 
+                                 "\\.txt", 
+                                 basename(f))))
+          })
         })
     })
   
--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # OSDRegistry
 
-[![R build status](https://github.com/ncss-tech/OSDRegistry/workflows/R-CMD-check/badge.svg)](https://github.com/ncss-tech/OSDRegistry/actions)
+[![Refresh](https://github.com/ncss-tech/OSDRegistry/workflows/refresh-osd/badge.svg)](https://github.com/ncss-tech/OSDRegistry/actions)
   
 Version control solution for [Official Series Descriptions](https://soilseries.sc.egov.usda.gov/) (OSDs). 
 
-Official "series" are soil types used by the USDA-NRCS and the National Cooperative Soil Survey program.
+Official "Soil Series" are detailed soil types used by the USDA-NRCS and the National Cooperative Soil Survey program. The Soil Series is the lowest level in the U.S. Soil Taxonomic system. Series are further subdivided into components and phases based on specific conditions within soil survey areas (SSA) and Major Land Resource Areas (MLRAs).
+
+Key differentia between series are encoded in the narrative portions of OSDs. OSDRegistry provides an open-source, version-controlled resource for tracking Series concepts as shown in OSDs. 
