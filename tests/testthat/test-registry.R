2a4d0d40815c9641d467cb75880ed29030d4ab4b
Andrew Gene Brown <andrew.g.brown@usda.gov>
2020-12-23 14:24:20 -0800

    Add validation and JSON structural parsing for all OSDs (#1)
    :wrench: Initial moves towards OSD Validation and parsing as JSON

--- tests/testthat/test-registry.R
+++ tests/testthat/test-registry.R
@@ -1,3 +1,5 @@
 test_that("Two region test works", {
+ test_download <- (length(list.files('OSD', "txt", recursive = TRUE)) > 0)
+ skip_if_not(test_download)
  expect_equal(refresh_registry(test = TRUE), 0)
 })
